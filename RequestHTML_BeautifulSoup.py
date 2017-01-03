376e69153f6640f7f9dc3ab4c42e5e77558987fe
Wlogsky <ss840429@gmail.com>
2017-01-03 16:21:37 +0800

    Get data of currency on website and print formated data

--- RequestHTML_BeautifulSoup.py
+++ RequestHTML_BeautifulSoup.py
@@ -5,18 +5,28 @@ request = requests.get('https://www.cathaybk.com.tw/cathaybk/personal_info07_pri
 request.encoding = 'big5'
 
 soup = BeautifulSoup(''.join(request.text), "lxml")
-print(request.encoding)
+#print(request.encoding)
 
-print(soup.prettify())  # 標準縮排格式
-print("\n")
-print(soup.title.string) # Title  
-print("\n")
-print(soup.p)	# <p> tag
-print(soup.a)	# <a> tag
-print("\n")
 
-for ps in soup.find_all('p'):	# Find all <p> tags
-	print( ps )
+# print(soup.prettify())  # 標準縮排格式
+# print("\n")
+# print(soup.title.string) # Title  
+# print("\n")
+# print(soup.p)	# <p> tag
+# print(soup.a)	# <a> tag
+# print("\n")
 
-for ps in soup.find_all('tr', bgcolor='#FFFFFF'):	# Find all <tr> where bgcolor='#FFFFFF'
-	print( ps,'\n' )
+
+for ps in soup.find_all('p'):				# Find all <p> tags
+	print( ps.find('b').string, end='    ')
+	for ps1 in ps('font'):
+		print( ps1.string, end='')			# Title
+	print('\n')
+
+for ps in soup('tr', bgcolor='#FFFFFF'):	# Find all <tr> where bgcolor='#FFFFFF'
+	for index, ps1 in enumerate(ps('td')):
+		print( '{:10}'.format(ps1.string.replace(' ', '')), end='\t')
+	print()
+
+
+
