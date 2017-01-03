d3d2c5a55d4e6869d1441ad9c6f0cdd5ecba0ce5
Wlogsky <ss840429@gmail.com>
2017-01-03 16:22:54 +0800

    Save formatted data into csv file

--- RequestHTML_BeautifulSoup.py
+++ RequestHTML_BeautifulSoup.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+import datetime
 
 request = requests.get('https://www.cathaybk.com.tw/cathaybk/personal_info07_print.asp')
 request.encoding = 'big5'
@@ -16,6 +18,7 @@ soup = BeautifulSoup(''.join(request.text), "lxml")
 # print(soup.a)	# <a> tag
 # print("\n")
 
+now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
 for ps in soup.find_all('p'):				# Find all <p> tags
 	print( ps.find('b').string, end='    ')
@@ -28,5 +31,12 @@ for ps in soup('tr', bgcolor='#FFFFFF'):	# Find all <tr> where bgcolor='#FFFFFF'
 		print( '{:10}'.format(ps1.string.replace(' ', '')), end='\t')
 	print()
 
+with open( str(now+'.csv'), 'w') as csvfile:				# Open csv file named by time
+	fieldnames = ['Currency', '幣別', 'Buy', 'Sell']
+	writer = csv.writer(csvfile)
+	writer.writerow( fieldnames )
 
+	for ps in soup('tr', bgcolor='#FFFFFF'):				# Find all <tr> where bgcolor='#FFFFFF'
+		row = ['{:10}'.format(ps1.string.replace(' ', '')) for ps1 in ps('td')]	
+		writer.writerow( row )
 
